4777a2a7e38dd9b77e7ce284d1234a7577a736b1
diademiemi <emilia@diademiemi.me>
2022-05-22 00:37:48 +0200

    Change POST to GET

--- src/main/java/me/diademeimi/queercryption/Queercryption.java
+++ src/main/java/me/diademeimi/queercryption/Queercryption.java
@@ -1,8 +1,8 @@
 package me.diademeimi.queercryption;
 
 import javax.ws.rs.Path;
-import javax.ws.rs.POST;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -13,7 +13,7 @@ import javax.json.*;
 @Path("queercryption")
 public class Queercryption {
 
-    @POST
+    @GET
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public String queercryption(String string) {
